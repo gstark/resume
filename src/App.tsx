d6fe0338a8bbfd9787dae197ef5d527b5b337e5d
Gavin Stark <gavin@gstark.com>
2021-09-18 14:41:44 -0400

    Adds a follow-me-on-github

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,22 @@ import { Skills } from './Skills'
 export function App() {
   return (
     <>
+      <div
+        className="
+          print:hidden
+          w-full bg-gray-700 text-gray-300 p-2 border-4 border-gray-300
+
+          xl:block xl:fixed xl:w-auto xl:top-12 xl:-right-16 xl:transform xl:rotate-45 xl:bg-gray-700 xl:text-gray-300 xl:p-2 xl:border-4 xl:border-gray-300"
+      >
+        <a
+          className="block w-full xl:w-60 text-sm text-center"
+          href="https://github.com/gstark"
+          data-ribbon="Find me on GitHub"
+          title="Find me on GitHub"
+        >
+          Find me on GitHub
+        </a>
+      </div>
       <div className="mx-6 xl:mx-64 mb-20">
         <section className="grid grid-cols-12 border-b-4 sticky top-0 pt-8 pb-2 bg-gray-50 z-10">
           <p className="col-span-12 md:col-span-10 text-3xl">Gavin Stark</p>
