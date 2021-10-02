345ff2042a441eedb8dc92b9774c7a40d1888cfc
Gavin Stark <gavin@gstark.com>
2021-10-02 16:39:23 -0400

    Restyle header details and links

--- src/Header.tsx
+++ src/Header.tsx
@@ -15,19 +15,21 @@ export function Header({
   }
 }) {
   return (
-    <section className="grid grid-cols-12 border-b-4 sticky top-0 pt-8 pb-2 bg-gray-50 z-10">
-      <p className="col-span-12 md:col-span-10 print:col-span-10 text-3xl">
-        {contact.name}
-      </p>
-      <div className="col-span-12 md:col-span-2 print:col-span-2 leading-snug md:text-right">
-        <p>
-          <a href="tel:727-560-3116">{resume.contact.phone}</a>
-        </p>
-        <p>
-          <a href={`mailto:${resume.contact.email}`}>{resume.contact.email}</a>
-        </p>
-        <p>
-          <a href={`https://www.linkedin.com/in/${resume.contact.linkedin}`}>
+    <section className="border-b-4 sticky top-0 pt-8 pb-2 bg-gray-50 z-10">
+      <p className="col-span-12 text-3xl">{contact.name}</p>
+      <div className="flex justify-between leading-snug my-3">
+        <div>
+          <a className="block" href={`mailto:${resume.contact.email}`}>
+            {resume.contact.email}
+          </a>
+          <a className="block" href="tel:727-560-3116">
+            {resume.contact.phone}
+          </a>
+        </div>
+        <div className="w-100">
+          <a
+            className="block"
+            href={`https://www.linkedin.com/in/${resume.contact.linkedin}`}>
             <img
               className="inline pr-1"
               style={{ height: '1rem' }}
@@ -35,9 +37,9 @@ export function Header({
             />
             {resume.contact.linkedin}
           </a>
-        </p>
-        <p>
-          <a href={`https://github.com/${resume.contact.github}`}>
+          <a
+            className="block"
+            href={`https://github.com/${resume.contact.github}`}>
             <img
               className="inline pr-1"
               style={{ height: '1rem' }}
@@ -45,7 +47,7 @@ export function Header({
             />
             {resume.contact.github}
           </a>
-        </p>
+        </div>
       </div>
     </section>
   )
